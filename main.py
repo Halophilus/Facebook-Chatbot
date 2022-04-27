121755cc25d8671b0ee5cf2a2ef5f1549f5c3e0a
Ethan McCue <btmhenshaw@gmail.com>
2022-04-27 18:09:44 -0400

    Add means of parsing Facebook statuses from aggregate JSON

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 import random
+import json
+import flatdict
+
+with open('posts/your_posts_1.json','r') as f:
+    print()
+    rawPosts = json.load(f)
+    title = "post"
+    flat_list = []
+    for elements in rawPosts:
+        if type(elements) is list or dict:
+            for item in elements:
+                flat_list.append(elements)
+    flat_list2 = []
+    for dictionary in flat_list:
+        if 'data' in dictionary:
+            tempList = dictionary['data']
+            for dictionaries in tempList:
+                if 'post' in dictionaries:
+                    flat_list2.append(dictionaries['post'])
+    postString = ""
+    purgedPosts = list(set(flat_list2))
+    for post in purgedPosts:
+        postString = postString + post + ' '
+
+print(postString)
 
 sample = open("data.txt", "r", errors="ignore")
 data = sample.read()
