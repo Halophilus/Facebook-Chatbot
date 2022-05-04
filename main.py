309c4b9981302b927b4b5d9dae89bfafdbba7f72
Ethan McCue <btmhenshaw@gmail.com>
2022-05-04 18:37:29 -0400

    Add discoverFiles, pullMessages, removeAlert, isBlacklisted, parseComments, and parseStatuses
    Means of cleaning up raw JSON data into legible lists

--- main.py
+++ main.py
@@ -1,29 +1,85 @@
 import random
 import json
 import flatdict
+import os
+import sys
 
-with open('posts/your_posts_1.json','r') as f:
-    print()
-    rawPosts = json.load(f)
-    title = "post"
-    flat_list = []
-    for elements in rawPosts:
-        if type(elements) is list or dict:
-            for item in elements:
-                flat_list.append(elements)
-    flat_list2 = []
-    for dictionary in flat_list:
-        if 'data' in dictionary:
-            tempList = dictionary['data']
-            for dictionaries in tempList:
-                if 'post' in dictionaries:
-                    flat_list2.append(dictionaries['post'])
-    postString = ""
-    purgedPosts = list(set(flat_list2))
-    for post in purgedPosts:
-        postString = postString + post + ' '
+name = input("Enter your name: ")
 
-print(postString)
+def discoverFiles(dir):
+    r = []
+    for root, dirs, files in os.walk(dir):
+        for name in files:
+            r.append(os.path.join(root,name))
+    s = []
+    for paths in r:
+        if paths.endswith('.json'):
+            s.append(paths)
+    return s
+
+
+def pullMessages(paths):
+    messageList = []
+    for file in paths:
+        with open(file, 'r') as f:
+            messages = json.load(f)
+            rawMessages = messages['messages']
+            for i in rawMessages:
+                if i['sender_name'] == name and 'content' in i:
+                    messageList.append(i['content'])
+    return messageList
+
+def removeAlert(messy):
+    y = []
+    for content in messy:
+      if isBlacklisted(content):
+          y.append(content)
+    return y
+
+def isBlacklisted(content):
+    blacklist = ['You sent a','You voted for','You started a video','You set the ','You scored','You responded ','You named','You left ','You joined ','You invited ','You changed the','You called']
+    for figs in blacklist:
+        if content.startswith(figs):
+            return False
+    return True
+
+def parseComments(dir):
+    with open(dir, 'r') as e:
+        comments = json.load(e)
+        rawComments = comments['comments']
+        flat_list = []
+        for elements in rawComments:
+            if 'data' in elements:
+                elementsData = elements['data']
+                for i in elementsData:
+                    if 'comment' in i:
+                        flatComment = i['comment']
+                        if 'comment' in flatComment:
+                            flat_list.append(flatComment['comment']) 
+        return flat_list
+
+
+def parseStatuses(dir):
+    with open(dir,'r') as f:
+        rawPosts = json.load(f)
+        flat_list = []
+        for elements in rawPosts:
+            if 'data' in elements:
+                elementsData = elements['data']
+                for i in elementsData:
+                    if 'post' in i:
+                        flat_list.append(i['post'])
+    return flat_list
+
+if __name__ == '__main__':
+    megaList = []
+    comments = parseComments('comments/comments.json')
+    messages = removeAlert(pullMessages(discoverFiles('messages/inbox')))
+    statuses = parseStatuses('posts/your_posts_1.json')
+    megaList.append(comments)
+    megaList.append(messages)
+    megaList.append(statuses)
+    print(megaList)
 
 sample = open("data.txt", "r", errors="ignore")
 data = sample.read()
@@ -78,5 +134,4 @@ def generateStatement(wordCount):
 
 for _ in range(1):
     print(generateStatement(wordCount))
-   
    
